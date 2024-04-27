ecdf4781848a7e0fa8f88cb9b4f1c647da315a9a
nestorrdmgz <nestorrdmgz@gmail.com>
2024-04-27 19:50:42 +0100

    Sign up terminado, falta el fondo pq la foto la tenemos que bajar de firebase
    También falta hacer el ruteo a las paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent} from "./views/login/login.component";
+import {SignUpComponent} from "./views/sign-up/sign-up.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-      RouterOutlet,
-      LoginComponent
+    RouterOutlet,
+    LoginComponent,
+    SignUpComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
